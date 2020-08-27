901c61fe2cf676cf58bcdecbb9bfad00b9afceeb
Kaiyrkhan <kaiyrkhan0serik@gmail.com>
2020-08-28 00:34:04 +0600

    addpages

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 
 const express = require('express');
+const _ = require("lodash");
 const app = express();
 
 
@@ -10,10 +11,16 @@ app.set("view engine","ejs");
 app.use(express.static('public'));
 
 
-app.get("/",(req,res)=>{
-  res.render("home");
+const cards = [{title:"Washing machine",description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"},{title:"Washing machine",description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"},{title:"Washing machine",description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"}]
+
+app.get("/",function(req,res){
+  res.render("home",{pageTitle:"Home",cards:cards});
 });
 
+
+app.get("/good",function(req,res){
+  res.render("good",{pageTitle:"good",goodName:cards[0].title,goodDescription:cards[0].description});
+});
 
 
 app.listen(3000,()=>{
