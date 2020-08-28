d2f353fa807ce40cddeaa46db43b21df7b58b8d2
Kaiyrkhan <kaiyrkhan0serik@gmail.com>
2020-08-29 01:38:33 +0600

    add functionality(add product,select product)

--- app.js
+++ app.js
@@ -3,7 +3,11 @@
 
 const express = require('express');
 const _ = require("lodash");
+const mongoose = require("mongoose");
 const app = express();
+
+
+mongoose.connect('mongodb://localhost:27017/ecommerce', {useNewUrlParser: true,useUnifiedTopology: true});
 
 
 const bodyParser = require('body-parser');
@@ -12,35 +16,72 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('public'));
 
 
-const cards = [{title:"Washing machine",
-        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry."},
-        {title:"TV",
-        description:"At nec cibo dolore honestatis."},
-        {title:"Fridge",
-        description:"Per zril temporibus in, duo nullam invidunt molestiae eu, te per nullam minimum."}]
+const goodsSchema = new mongoose.Schema({
+  title:String,
+  description:String,
+  images:[String]
+})
+
+const Good = mongoose.model("Good",goodsSchema);
 
 
-var card = "TV";
+var defaultCard = {title:"Timberland Boots",
+  description:"In 1973, the small town of Newmarket in New Hampshire played host to the birth of an American legend.",
+  images:["https://images.timberland.com/is/image/TimberlandEU/10061713-hero?wid=720&hei=720&fit=constrain,1&qlt=85,1&op_usm=1,1,6,0"]
+};
+
 app.get("/",function(req,res){
-  res.render("home",{pageTitle:"Home",cards:cards});
+  Good.find(function(err,products){
+    res.render("home",{cards:products});
+  });
 });
 
 
 app.get("/good",function(req,res){
-  cards.forEach(function(element){
-    if(element.title === card){
-      card = element
-    }
-  });
-  res.render("good",{pageTitle:"Good",goodName:card.title,goodDescription:card.description});
+  res.render("good",{card:defaultCard});
 });
 
 
-app.post("/good",function(req,res){
-  card = req.body.card;
-  res.redirect("/good");
+app.post("/goodDetailed",function(req,res){
+  const cardTitle = req.body.card;
+
+  Good.find({title:cardTitle},function(err,result){
+    if(err){
+      console.log(err);
+    }else{
+      defaultCard = result[0];
+      console.log(defaultCard);
+      res.redirect("/good");
+    }
+  });
+});
+
+
+app.get("/add",function(req,res){
+  res.render("add");
+});
+
+app.post("/add",function(req,res){
+  const body = req.body;
+  const good = new Good({
+    title:_.capitalize(body.title),
+    description:body.description,
+    images:[body.imageURL1,body.imageURL2,body.imageURL3]
+  })
+
+
+  good.save();
+
+  res.redirect("/");
 });
 
 app.listen(3000,()=>{
   console.log("Server is running on port 3000");
 });
+
+
+
+// { "_id" : ObjectId("5f4940114ee9a45273af916e"),
+// "title" : "Timberland Boots",
+// "description" : "In 1973, the small town of Newmarket in New Hampshire played host to the birth of an American legend.",
+//"image" : "https://images.timberland.com/is/image/TimberlandEU/10061713-hero?wid=720&hei=720&fit=constrain,1&qlt=85,1&op_usm=1,1,6,0", "__v" : 0 }
