fcf6675fded8dfe5e2742736533dd6712e5cfed2
Kaiyrkhan <kaiyrkhan0serik@gmail.com>
2020-08-29 22:30:45 +0600

    add PORT

--- app.js
+++ app.js
@@ -109,6 +109,6 @@ app.post("/signup",function(req,res){
 app.get("/signin",function(req,res){
   res.render("signin");
 });
-app.listen(3000,()=>{
+app.listen(process.env.PORT || 3000,()=>{
   console.log("Server is running on port 3000");
 });
